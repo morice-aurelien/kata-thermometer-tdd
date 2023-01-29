27f9a0cac7d154fd4c23aa316ed09d67c57fdcd8
Aurélien Morice <aurelien.morice@gmail.com>
2023-01-29 14:36:21 -0500

    2nd iteration - RED step

--- test/unit/thermometer.spec.ts
+++ test/unit/thermometer.spec.ts
@@ -4,4 +4,8 @@ describe("Kata Thermometer using TDD", () => {
     it('should return 0 when no temperature is provided', function () {
         expect(thermometer()).toBe(0);
     });
+    it('should return a temperature when one temperature is provided', function () {
+        const temperature = 10;
+        expect(thermometer(temperature)).toBe(10);
+    });
 });
