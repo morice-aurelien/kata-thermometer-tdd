f0a4e265718637e1fc67a0dcbcb5ee4d536f4125
Aurélien Morice <aurelien.morice@gmail.com>
2023-02-03 22:18:12 -0500

    5th iteration - RED step

--- test/unit/thermometer.spec.ts
+++ test/unit/thermometer.spec.ts
@@ -13,10 +13,14 @@ describe("Kata Thermometer using TDD", () => {
         const temperature2 = 5;
         expect(thermometer(temperature1, temperature2)).toBe(5);
     });
-    it('should return the lowest temperature when three temperatures are provided in reverse order', function () {
+    it('should return the lowest temperature when three temperatures are provided', function () {
         const temperature1 = 10;
         const temperature2 = 5;
         const temperature3 = 1;
         expect(thermometer(temperature1, temperature2, temperature3)).toBe(1);
+    });
+    it('should throw an error when more thant 10000 temperatures are provided', function () {
+        const temperatures = Array.from({length: 10001}, () => 1);
+        expect(() => thermometer(...temperatures)).toThrowError("Too many temperatures");
     });
 });
