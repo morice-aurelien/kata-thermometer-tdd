68edc9598f1e36175ef41860bff89705662db5d3
Aurélien Morice <aurelien.morice@gmail.com>
2023-02-03 22:32:45 -0500

    6th iteration - GREEN step

--- src/thermometer-tdd.ts
+++ src/thermometer-tdd.ts
@@ -5,5 +5,12 @@ export function thermometer(...temperatures: number []): number {
     if (temperatures.length > 10000) {
         throw new Error("Too many temperatures");
     }
-    return Math.min(...temperatures);
+
+    const distanceTemperatureOrderedList = temperatures.map(temperature => ({
+        temperature, distance: Math.abs(temperature)
+    })).sort((a, b) => a.distance - b.distance);
+
+    return  distanceTemperatureOrderedList[0].temperature > 0 ?
+            distanceTemperatureOrderedList[0].temperature :
+            distanceTemperatureOrderedList[1].temperature;
 }
