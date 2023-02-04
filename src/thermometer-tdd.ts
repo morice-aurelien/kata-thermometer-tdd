14562b773be4a74264b5c9591b6d327b30916422
Aurélien Morice <aurelien.morice@gmail.com>
2023-02-03 22:07:52 -0500

    4th iteration - GREEN step

--- src/thermometer-tdd.ts
+++ src/thermometer-tdd.ts
@@ -1,5 +1,11 @@
 export function thermometer(temperature?: number, temperature2?: number, temperature3?:number): number {
-    if(temperature === undefined) return temperature2 || 0;
-    if(temperature2 === undefined) return temperature || 0;
-    return Math.min(temperature, temperature2);
+    if(temperature === undefined && temperature2 === undefined && temperature3 === undefined) return 0;
+    if(temperature === undefined && temperature2 === undefined) return temperature3 || 0;
+    if(temperature2 === undefined && temperature3 === undefined) return temperature || 0;
+    if(temperature === undefined && temperature3 === undefined) return temperature2 || 0;
+    if(temperature === undefined) return Math.min(temperature2||0, temperature3||0);
+    if(temperature2 === undefined) return Math.min(temperature, temperature3||0);
+    if(temperature3 === undefined) return Math.min(temperature, temperature2);
+
+    return Math.min(temperature, temperature2, temperature3);
 }
