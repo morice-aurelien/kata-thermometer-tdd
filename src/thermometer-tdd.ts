a9c9a9f3682cb0abdb584291fd2059b867cd830c
Aurélien Morice <aurelien.morice@gmail.com>
2023-02-03 21:59:09 -0500

    3rd iteration - GRENN step

--- src/thermometer-tdd.ts
+++ src/thermometer-tdd.ts
@@ -1,3 +1,5 @@
 export function thermometer(temperature?: number, temperature2?: number): number {
-    return temperature || 0;
+    if(temperature === undefined) return temperature2 || 0;
+    if(temperature2 === undefined) return temperature || 0;
+    return Math.min(temperature, temperature2);
 }
