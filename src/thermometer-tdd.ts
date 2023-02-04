e8afbf1341181dfefed34bf90c7283a9dc4cf51f
Aurélien Morice <aurelien.morice@gmail.com>
2023-02-03 22:19:32 -0500

    5th iteration - GREEN step

--- src/thermometer-tdd.ts
+++ src/thermometer-tdd.ts
@@ -1,6 +1,11 @@
+import {loadConfig} from "tsconfig-paths";
+
 export function thermometer(...temperatures: number []): number {
     if (temperatures.length === 0) {
         return 0;
+    }
+    if (temperatures.length > 10000) {
+        throw new Error("Too many temperatures");
     }
     return Math.min(...temperatures);
 }
