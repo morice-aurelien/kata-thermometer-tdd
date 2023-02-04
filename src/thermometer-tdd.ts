a1df4384b9ca0bb621c19111a47545ae2fed183d
Aurélien Morice <aurelien.morice@gmail.com>
2023-02-03 22:10:17 -0500

    4th iteration - REFACTOR step

--- src/thermometer-tdd.ts
+++ src/thermometer-tdd.ts
@@ -1,11 +1,6 @@
-export function thermometer(temperature?: number, temperature2?: number, temperature3?:number): number {
-    if(temperature === undefined && temperature2 === undefined && temperature3 === undefined) return 0;
-    if(temperature === undefined && temperature2 === undefined) return temperature3 || 0;
-    if(temperature2 === undefined && temperature3 === undefined) return temperature || 0;
-    if(temperature === undefined && temperature3 === undefined) return temperature2 || 0;
-    if(temperature === undefined) return Math.min(temperature2||0, temperature3||0);
-    if(temperature2 === undefined) return Math.min(temperature, temperature3||0);
-    if(temperature3 === undefined) return Math.min(temperature, temperature2);
-
-    return Math.min(temperature, temperature2, temperature3);
+export function thermometer(...temperatures: number []): number {
+    if (temperatures.length === 0) {
+        return 0;
+    }
+    return Math.min(...temperatures);
 }
